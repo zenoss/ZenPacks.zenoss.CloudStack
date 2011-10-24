b1fab87d8acbe1f9c4d6b638d746107b7bd73722
Chet Luther <cluther@zenoss.com>
2011-10-23 23:18:28 -0500

    Add modeling of zones.

--- ZenPacks/zenoss/CloudStack/modeler/plugins/zenoss/CloudStack.py
+++ ZenPacks/zenoss/CloudStack/modeler/plugins/zenoss/CloudStack.py
@@ -34,9 +34,6 @@ class CloudStack(PythonPlugin):
         'zCloudStackSecretKey',
         )
 
-    def __init__(self):
-        self._data = {}
-
     def collect(self, device, unused):
         if not device.zCloudStackURL:
             LOG.error('zCloudStackURL is not set. Not discovering')
@@ -66,13 +63,12 @@ class CloudStack(PythonPlugin):
         return d
 
     def _combine(self, results):
-        """Combine the results for all API calls into one datastructure."""
+        all_data = {}
+
         for success, result in results:
             if not success:
                 LOG.error("API Error: %s", result.getErrorMessage())
                 return None
-
-            data = None
 
             try:
                 data = json.loads(result)
@@ -81,8 +77,45 @@ class CloudStack(PythonPlugin):
                 LOG.exception(ex)
                 return None
 
-            self._data.update(data)
+            all_data.update(data)
 
-    def process(self, devices, results, unused):
-        import pdb; pdb.set_trace()
-        return None
+        return all_data
+
+    def process(self, device, results, unused):
+        zone_maps = []
+
+        zones_response = results.get('listzonesresponse', None)
+        if zones_response is None:
+            LOG.error('No listZones response from API')
+            return None
+
+        zones_count = zones_response.get('count', None)
+        if zones_count is not None:
+            LOG.info('Found %s zones.', zones_count)
+
+        for zone in zones_response.get('zone', []):
+            zone_id = self.prepId('zone%s' % zone['id'])
+
+            zone_maps.append(ObjectMap(data=dict(
+                id=zone_id,
+                title=zone.get('name', zone_id),
+                cloudstack_id=zone['id'],
+                allocation_state=zone.get('allocationstate', ''),
+                guest_cidr_address=zone.get('guestcidraddress', ''),
+                dhcp_provider=zone.get('dhcpprovider', ''),
+                dns1=zone.get('dns1', ''),
+                dns2=zone.get('dns2', ''),
+                internal_dns1=zone.get('internaldns1', ''),
+                internal_dns2=zone.get('internaldns2', ''),
+                network_type=zone.get('networktype', ''),
+                security_groups_enabled=zone.get('securitygroupsenabled', ''),
+                vlan=zone.get('vlan', ''),
+                zone_token=zone.get('zonetoken', ''),
+                )))
+
+        zones_map = RelationshipMap(
+            relname='zones',
+            modname='ZenPacks.zenoss.CloudStack.Zone',
+            objmaps=zone_maps)
+
+        return zones_map
