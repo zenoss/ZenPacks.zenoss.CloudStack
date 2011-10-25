0e21152eb64abdcab24e5614780b98285de71e56
Chet Luther <cluther@zenoss.com>
2011-10-25 11:07:41 -0500

    Add API (Info) interfaces and adapter factories.

---
+++ ZenPacks/zenoss/CloudStack/info.py
@@ -0,0 +1,107 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2011, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 or (at your
+# option) any later version as published by the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+from zope.interface import implements
+
+from Products.Zuul.infos import ProxyProperty
+from Products.Zuul.infos.device import DeviceInfo
+from Products.Zuul.infos.component import ComponentInfo
+
+from ZenPacks.zenoss.CloudStack.interfaces import (
+    ICloudInfo, IZoneInfo, IPodInfo, IClusterInfo, IHostInfo)
+
+
+class CloudInfo(DeviceInfo):
+    """Cloud API (Info) adapter factory."""
+
+    implements(ICloudInfo)
+
+
+class BaseComponentInfo(ComponentInfo):
+    """Abstract base component API (Info) adapter factory."""
+
+    allocation_state = ProxyProperty('allocation_state')
+
+    @property
+    def entity(self):
+        return {
+            'uid': self._object.getPrimaryUrlPath(),
+            'name': self._object.titleOrId(),
+            }
+
+    @property
+    def icon(self):
+        return self._object.getIconPath()
+
+
+class ZoneInfo(BaseComponentInfo):
+    """Zone API (Info) adapter factory."""
+
+    implements(IZoneInfo)
+
+    guest_cidr_address = ProxyProperty('guest_cidr_address')
+    dhcp_provider = ProxyProperty('dhcp_provider')
+    network_type = ProxyProperty('network_type')
+    security_groups_enabled = ProxyProperty('security_groups_enabled')
+    vlan = ProxyProperty('vlan')
+    zone_token = ProxyProperty('zone_token')
+
+    @property
+    def public_dns(self):
+        return ', '.join((self._object.dns1, self._object.dns2))
+
+    @property
+    def internal_dns(self):
+        return ', '.join((
+            self._object.internal_dns1, self._object.internal_dns1))
+
+
+class PodInfo(BaseComponentInfo):
+    """Pod API (Info) adapter factory."""
+
+    implements(IPodInfo)
+
+    netmask = ProxyProperty('netmask')
+    gateway = ProxyProperty('gateway')
+
+    @property
+    def ip_range(self):
+        return "%s - %s" % (self._object.start_ip, self._object.end_ip)
+
+
+class ClusterInfo(BaseComponentInfo):
+    """Cluster API (Info) adapter factory."""
+
+    implements(IClusterInfo)
+
+    cluster_type = ProxyProperty('cluster_type')
+    hypervisor_type = ProxyProperty('hypervisor_type')
+    managed_state = ProxyProperty('managed_state')
+
+
+class HostInfo(BaseComponentInfo):
+    """Host API (Info) adapter factory."""
+
+    implements(IHostInfo)
+
+    host_type = ProxyProperty('host_type')
+    hypervisor = ProxyProperty('hypervisor')
+    host_version = ProxyProperty('host_version')
+    capabilities = ProxyProperty('capabilities')
+    host_state = ProxyProperty('host_state')
+    created = ProxyProperty('created')
+    host_tags = ProxyProperty('host_tags')
+    ip_address = ProxyProperty('ip_address')
+    host_events = ProxyProperty('host_events')
+    local_storage_active = ProxyProperty('local_storage_active')
+    management_server_id = ProxyProperty('management_server_id')
--- ZenPacks/zenoss/CloudStack/interfaces.py
+++ ZenPacks/zenoss/CloudStack/interfaces.py
@@ -11,9 +11,86 @@
 #
 ###########################################################################
 
+from Products.Zuul.form import schema
 from Products.Zuul.interfaces import IFacade
+from Products.Zuul.interfaces import IDeviceInfo
+from Products.Zuul.interfaces.component import IComponentInfo
+from Products.Zuul.utils import ZuulMessageFactory as _t
+
+# In Zenoss 3 we mistakenly mapped TextLine to Zope's multi-line text
+# equivalent and Text to Zope's single-line text equivalent. This was
+# backwards so we flipped their meanings in Zenoss 4. The following block of
+# code allows the ZenPack to work properly in Zenoss 3 and 4.
+
+# Until backwards compatibility with Zenoss 3 is no longer desired for your
+# ZenPack it is recommended that you use "SingleLineText" and "MultiLineText"
+# instead of schema.TextLine or schema.Text.
+from Products.ZenModel.ZVersion import VERSION as ZENOSS_VERSION
+from Products.ZenUtils.Version import Version
+
+if Version.parse('Zenoss %s' % ZENOSS_VERSION) >= Version.parse('Zenoss 4'):
+    SingleLineText = schema.TextLine
+    MultiLineText = schema.Text
+else:
+    SingleLineText = schema.Text
+    MultiLineText = schema.TextLine
 
 
 class ICloudStackFacade(IFacade):
     def add_cloudstack(self, url, api_key, secret_key):
         """Add CloudStack cloud."""
+
+
+class ICloudInfo(IDeviceInfo):
+    """Interface for Cloud API (Info) adapter."""
+
+
+class IBaseComponentInfo(IComponentInfo):
+    """Abstract base component API (Info) adapter."""
+
+    allocation_state = SingleLineText(title=_t(u"Allocation State"))
+
+
+class IZoneInfo(IBaseComponentInfo):
+    """Interface for Zone API (Info) adapter."""
+
+    guest_cidr_address = SingleLineText(title=_t(u"Guest CIDR Address"))
+    dhcp_provider = SingleLineText(title=_t(u"DHCP Provider"))
+    public_dns = SingleLineText(title=_t(u"Public DNS"))
+    internal_dns = SingleLineText(title=_t(u"Internal DNS"))
+    network_type = SingleLineText(title=_t(u"Network Type"))
+    security_groups_enabled = schema.Bool(title=_t(u"Security Groups Enabled"))
+    vlan = SingleLineText(title=_t(u"VLAN Range"))
+    zone_token = SingleLineText(title=_t(u"Zone Token"))
+
+
+class IPodInfo(IBaseComponentInfo):
+    """Interface for Pod API (Info) adapter."""
+
+    ip_range = SingleLineText(title=_t(u"IP Range"))
+    netmask = SingleLineText(title=_t(u"Netmask"))
+    gateway = SingleLineText(title=_t(u"Gateway"))
+
+
+class IClusterInfo(IBaseComponentInfo):
+    """Interface for Cluster API (Info) adapter."""
+
+    cluster_type = SingleLineText(title=_t(u"Cluster Type"))
+    hypervisor_type = SingleLineText(title=_t(u"Hypervisor Type"))
+    managed_state = SingleLineText(title=_t(u"Managed State"))
+
+
+class IHostInfo(IBaseComponentInfo):
+    """Interface for Host API (Info) adapter."""
+
+    host_type = SingleLineText(title=_t(u"Host Type"))
+    hypervisor = SingleLineText(title=_t(u"Hypervisor"))
+    host_version = SingleLineText(title=_t(u"Version"))
+    capabilities = SingleLineText(title=_t(u"Capabilities"))
+    host_state = SingleLineText(title=_t(u"Host State"))
+    created = SingleLineText(title=_t(u"Created"))
+    host_tags = SingleLineText(title=_t(u"Host Tags"))
+    ip_address = SingleLineText(title=_t(u"IP Address"))
+    host_events = SingleLineText(title=_t(u"Event Types"))
+    local_storage_active = schema.Bool(title=_t(u"Local Storage Active"))
+    management_server_id = schema.Int(title=_t(u"Management Server Identifier"))
