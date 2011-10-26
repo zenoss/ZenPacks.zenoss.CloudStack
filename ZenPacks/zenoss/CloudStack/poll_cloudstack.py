17d354571b603b6ad3a60c07921eb296b16f14ad
Chet Luther <cluther@zenoss.com>
2011-10-26 16:32:21 -0500

    Add listHosts call to collector plugin.

--- ZenPacks/zenoss/CloudStack/poll_cloudstack.py
+++ ZenPacks/zenoss/CloudStack/poll_cloudstack.py
@@ -20,6 +20,7 @@ import tempfile
 import time
 
 from twisted.internet import reactor
+from twisted.internet.defer import DeferredList
 
 from utils import addLocalLibPath
 addLocalLibPath()
@@ -33,7 +34,7 @@ class CloudStackPoller(object):
         self._api_key = api_key
         self._secret_key = secret_key
 
-    def _getTempFilename(self):
+    def _temp_filename(self):
         target_hash = md5.md5('%s+%s+%s' % (
             self._url, self._api_key, self._secret_key)).hexdigest()
 
@@ -41,14 +42,14 @@ class CloudStackPoller(object):
             tempfile.gettempdir(),
             '.zenoss_cloudstack_%s' % target_hash)
 
-    def _cacheData(self, data):
-        tmpfile = self._getTempFilename()
+    def _cache_results(self, data):
+        tmpfile = self._temp_filename()
         tmp = open(tmpfile, 'w')
         json.dump(data, tmp)
         tmp.close()
 
-    def _loadData(self):
-        tmpfile = self._getTempFilename()
+    def _cached_results(self):
+        tmpfile = self._temp_filename()
         if not os.path.isfile(tmpfile):
             return None
 
@@ -64,23 +65,38 @@ class CloudStackPoller(object):
         return data
 
     def run(self):
-        def callback(result):
+        def callback(results):
+            if reactor.running:
+                reactor.stop()
+
             data = {'events': []}
 
-            try:
-                data.update(json.loads(result))
-            except Exception, ex:
-                data['events'].append(dict(
-                    severity=4,
-                    summary='error parsing API response',
-                    message='error parsing API response: %s' % ex,
-                    eventClassKey='cloudstack_parse_error',
-                    ))
+            for success, result in results:
+                if not success:
+                    error = result.getErrorMessage()
+                    data['events'].append(dict(
+                        severity=4,
+                        summary='CloudStack error: %s' % error,
+                        eventClassKey='cloudstack_error',
+                        ))
 
-                print json.dumps(data)
-                return
+                    print json.dumps(data)
+                    return
 
-            self._cacheData(result)
+                try:
+                    data.update(json.loads(result))
+                except Exception, ex:
+                    data['events'].append(dict(
+                        severity=4,
+                        summary='error parsing API response',
+                        message='error parsing API response: %s' % ex,
+                        eventClassKey='cloudstack_parse_error',
+                        ))
+
+                    print json.dumps(data)
+                    return
+
+            self._cache_results(results)
 
             data['events'].append(dict(
                 severity=0,
@@ -91,19 +107,9 @@ class CloudStackPoller(object):
 
             print json.dumps(data)
 
-        def errback(error):
-            data = {'events': []}
-            data['events'].append(dict(
-                severity=4,
-                summary='CloudStack error: %s' % error.getErrorMessage(),
-                eventClassKey='cloudstack_error',
-                ))
-
-            print json.dumps(data)
-
-        last_result = self._loadData()
-        if last_result is not None:
-            callback(last_result)
+        cached_results = self._cached_results()
+        if cached_results is not None:
+            callback(cached_results)
             return
 
         client = txcloudstack.Client(
@@ -111,9 +117,10 @@ class CloudStackPoller(object):
             self._api_key,
             self._secret_key)
 
-        client.listCapacity().addCallbacks(
-            callback=callback,
-            errback=errback).addBoth(lambda x: reactor.stop())
+        DeferredList((
+            client.listCapacity(),
+            client.listHosts(type="Routing"),
+            ), consumeErrors=True).addCallback(callback)
 
         reactor.run()
 
