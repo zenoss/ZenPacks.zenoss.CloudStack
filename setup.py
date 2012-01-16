4472a3282de4d3a493cb011746c595e5b04ada4c
Chet Luther <cluther@zenoss.com>
2012-01-16 16:05:34 -0600

    Bump to version 0.7.6.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CloudStack"
-VERSION = "0.7.4"
+VERSION = "0.7.6"
 AUTHOR = "Zenoss Labs"
 LICENSE = ""
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
