a71c2100787c921a123fd8cce3e4bb1e5a7e1682
Chet Luther <chet.luther@gmail.com>
2013-09-13 12:53:45 -0500

    Version 1.1.0 -> 1.1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CloudStack"
-VERSION = "1.1.0"
+VERSION = "1.1.1"
 AUTHOR = "Zenoss Labs <labs@zenoss.com>"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
