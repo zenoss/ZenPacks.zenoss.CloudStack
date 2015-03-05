8f06d155d80f36a32ff7780ebd8710116bf32ad1
Chet Luther <chet.luther@gmail.com>
2015-03-05 08:11:14 -0600

    Bump develop version: 1.1.3dev -> 1.2.0dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.zenoss.CloudStack"
-VERSION = "1.1.3dev"
+VERSION = "1.2.0dev"
 AUTHOR = "Zenoss Labs <labs@zenoss.com>"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
